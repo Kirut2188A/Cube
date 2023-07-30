7fd786fc69e4284c87c080276b68ac1812f2ca24
Tetherous <73020510+Kirut2188A@users.noreply.github.com>
2023-07-31 03:52:36 +0800

    Update script.js

--- script.js
+++ script.js
@@ -2,21 +2,36 @@ const cube = document.getElementById('cube');
 let isDragging = false;
 let previousX, previousY;
 
-cube.addEventListener('mousedown', (e) => {
+cube.addEventListener('mousedown', startDragging);
+cube.addEventListener('touchstart', startDragging);
+
+document.addEventListener('mousemove', rotateCube);
+document.addEventListener('touchmove', rotateCube);
+
+document.addEventListener('mouseup', stopDragging);
+document.addEventListener('touchend', stopDragging);
+
+function startDragging(e) {
   isDragging = true;
-  previousX = e.clientX;
-  previousY = e.clientY;
-});
+  previousX = e.clientX || e.touches[0].clientX;
+  previousY = e.clientY || e.touches[0].clientY;
+}
 
-document.addEventListener('mousemove', (e) => {
+function rotateCube(e) {
   if (!isDragging) return;
-  const deltaX = e.clientX - previousX;
-  const deltaY = e.clientY - previousY;
-  previousX = e.clientX;
-  previousY = e.clientY;
-  cube.style.transform = `rotateX(${deltaY * 0.5}deg) rotateY(${deltaX * 0.5}deg) ${cube.style.transform}`;
-});
 
-document.addEventListener('mouseup', () => {
+  const currentX = e.clientX || e.touches[0].clientX;
+  const currentY = e.clientY || e.touches[0].clientY;
+
+  const deltaX = currentX - previousX;
+  const deltaY = currentY - previousY;
+
+  previousX = currentX;
+  previousY = currentY;
+
+  cube.style.transform = `rotateX(${deltaY * 0.5}deg) rotateY(${deltaX * 0.5}deg) ${cube.style.transform}`;
+}
+
+function stopDragging() {
   isDragging = false;
-});
+}
